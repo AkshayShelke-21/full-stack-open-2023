95db5c587141614fababf18712af4e9c9b47e547
AkshayShelke21 <akshelke21@gmail.com>
2023-08-17 11:38:19 +0530

    Unicafe [Exercise 1.7]

--- part1/react-app/src/App.jsx
+++ part1/react-app/src/App.jsx
@@ -5,32 +5,39 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setAll(all + 1);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setAll(all + 1);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAll(all + 1);
   };
 
   return (
     <div>
       <div>
-        <p>Give Feedback</p>
+        <h1>Give Feedback</h1>
         <button onClick={handleGoodClick}>Good</button>
         <button onClick={handleNeutralClick}>Neutral</button>
         <button onClick={handleBadClick}>Bad</button>
       </div>
       <div>
-        <p>Statistics</p>
+        <h1>Statistics</h1>
         <p>Good : {good}</p>
         <p>Neutral : {neutral}</p>
         <p>Bad : {bad}</p>
+        <p>All : {all}</p>
+        <p>Average : {((good * 1) + (neutral * 0) + (bad * -1))/all}</p>
+        <p>Positive : { (good/all) * 100} %</p>
       </div>
     </div>
   );
