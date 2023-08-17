d1d930eb8bce247b06784857e6118382ea823221
AkshayShelke21 <akshelke21@gmail.com>
2023-08-17 12:20:57 +0530

    Unicafe [Exercise 1.8]

--- part1/react-app/src/App.jsx
+++ part1/react-app/src/App.jsx
@@ -1,25 +1,43 @@
 import { useState } from "react";
 
+// eslint-disable-next-line react/prop-types
+const Statistics = ({ good, bad, neutral }) => {
+  const all = good + bad + neutral;
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good : {good}</p>
+      <p>Neutral : {neutral}</p>
+      <p>Bad : {bad}</p>
+      <p>All : {all}</p>
+      <p>Average : {(good * 1 + neutral * 0 + bad * -1) / all}</p>
+      <p>Positive : {(good / all) * 100} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
 
+  let all = 0;
+  
   const handleGoodClick = () => {
     setGood(good + 1);
-    setAll(all + 1);
+    all = all + 1;
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-    setAll(all + 1);
+    all = all + 1;
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
-    setAll(all + 1);
+    all = all + 1;
   };
 
   return (
@@ -30,15 +48,7 @@ const App = () => {
         <button onClick={handleNeutralClick}>Neutral</button>
         <button onClick={handleBadClick}>Bad</button>
       </div>
-      <div>
-        <h1>Statistics</h1>
-        <p>Good : {good}</p>
-        <p>Neutral : {neutral}</p>
-        <p>Bad : {bad}</p>
-        <p>All : {all}</p>
-        <p>Average : {((good * 1) + (neutral * 0) + (bad * -1))/all}</p>
-        <p>Positive : { (good/all) * 100} %</p>
-      </div>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   );
 };
