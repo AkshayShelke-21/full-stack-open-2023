802d2176457caab590a4f6a6e1537a8e3e94e273
AkshayShelke21 <akshelke21@gmail.com>
2023-08-17 11:20:28 +0530

    Unicafe [Exercise 1.6]

--- part1/react-app/src/App.jsx
+++ part1/react-app/src/App.jsx
@@ -1,69 +1,37 @@
-/* eslint-disable react/prop-types */
-
-const Header = (props) => {
-  
-  return (
-    <div>
-      <p>{props.course}</p>
-    </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercise}
-      </p>
-    </>
-  )
-};
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
-    </>
-  );
-};
-
-
-
-const Total = (props) => {
-    return(
-      <div>
-        <p>Number of exercises {props.total}</p>
-      </div>
-    )
-}
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+      <div>
+        <p>Give Feedback</p>
+        <button onClick={handleGoodClick}>Good</button>
+        <button onClick={handleNeutralClick}>Neutral</button>
+        <button onClick={handleBadClick}>Bad</button>
+      </div>
+      <div>
+        <p>Statistics</p>
+        <p>Good : {good}</p>
+        <p>Neutral : {neutral}</p>
+        <p>Bad : {bad}</p>
+      </div>
     </div>
   );
 };
